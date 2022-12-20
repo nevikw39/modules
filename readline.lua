6614081ca2eda5f9e73b4050e0cbbdc9a4fb1a7b
nevikw39 <nevikw39@lgn301.nchc-2020-hpc>
2022-12-20 16:06:50 +0800

    socat

---
+++ readline.lua
@@ -0,0 +1,4 @@
+prepend_path("PATH", "/home/nevikw39/opt/readline/bin")
+prepend_path("CPATH","/home/nevikw39/opt/readline/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/readline/lib")
+prepend_path("MANPATH", "/home/nevikw39/opt/readline/share/man")
---
+++ socat.lua
@@ -0,0 +1,4 @@
+load("readline")
+
+prepend_path("PATH", "/home/nevikw39/opt/socat/bin")
+prepend_path("MANPATH", "/home/nevikw39/opt/socat/share/man")
