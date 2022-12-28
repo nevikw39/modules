e4b94de54ab2d9bfd00d96365a55d9814c78908e
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-28 21:36:45 +0800

    libFTDI

---
+++ libftdi.lua
@@ -0,0 +1,8 @@
+load("libusb")
+load("libconfuse")
+
+prepend_path("PATH", "/home/nevikw39/opt/libftdi/bin")
+prepend_path("CPATH","/home/nevikw39/opt/libftdi/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/libftdi/lib64")
+prepend_path("PKG_CONFIG_PATH","/home/nevikw39/opt/libftdi/lib64/pkgconfig")
+
