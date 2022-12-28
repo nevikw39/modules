01f81fd2897bb7203d5834b3030e82020044d868
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-28 23:27:14 +0800

    libevent

---
+++ libevent.lua
@@ -0,0 +1,4 @@
+prepend_path("PATH", "/home/nevikw39/opt/libevent/bin")
+prepend_path("CPATH","/home/nevikw39/opt/libevent/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/libevent/lib")
+
