9ab9c6103bd7eaf521a4d04a306b69aca7f7d529
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-29 02:14:43 +0800

    numactl

---
+++ numactl.lua
@@ -0,0 +1,6 @@
+prepend_path("PATH", "/home/nevikw39/opt/numactl/bin")
+prepend_path("CPATH","/home/nevikw39/opt/numactl/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/numactl/lib")
+prepend_path("PKG_CONFIG_PATH","/home/nevikw39/opt/numactl/lib/pkgconfig")
+prepend_path("MANPATH", "/home/nevikw39/opt/numactl/share/man")
+
