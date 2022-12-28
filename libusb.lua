cef1074dd98efbe7a3c7765c676fe74ad0bffc4f
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-28 17:21:23 +0800

    libusb

---
+++ libusb.lua
@@ -0,0 +1,4 @@
+prepend_path("CPATH","/home/nevikw39/opt/libusb/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/libusb/lib")
+prepend_path("PKG_CONFIG_PATH","/home/nevikw39/opt/libusb/lib/pkgconfig")
+
