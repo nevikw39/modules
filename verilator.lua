887b7450ceecb6b17d31a74e6a3dbaa42b3f05dd
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-29 02:26:51 +0800

    Verilator

---
+++ verilator.lua
@@ -0,0 +1,8 @@
+load("ccache")
+load("gperftools")
+load("numactl")
+
+prepend_path("PATH", "/home/nevikw39/opt/verilator/bin")
+prepend_path("MANPATH", "/home/nevikw39/opt/verilator/share/man")
+prepend_path("PKG_CONFIG_PATH","/home/nevikw39/opt/verilator/share/pkgconfig")
+
