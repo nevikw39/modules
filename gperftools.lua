0e6fad2d972e4187b2825ee8278fd2a5d14b1471
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-29 02:09:31 +0800

    Google Performance Tools

---
+++ gperftools.lua
@@ -0,0 +1,6 @@
+prepend_path("PATH", "/home/nevikw39/opt/gperftools/bin")
+prepend_path("CPATH","/home/nevikw39/opt/gperftools/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/gperftools/lib")
+prepend_path("PKG_CONFIG_PATH","/home/nevikw39/opt/gperftools/lib/pkgconfig")
+prepend_path("MANPATH", "/home/nevikw39/opt/gperftools/share/man")
+
