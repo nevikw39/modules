5dc76fd035574c29f5097043e42a1f3825d38954
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-28 18:19:27 +0800

    libConfuse

---
+++ libconfuse.lua
@@ -0,0 +1,5 @@
+prepend_path("CPATH","/home/nevikw39/opt/libconfuse/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/libconfuse/lib")
+prepend_path("MANPATH", "/home/nevikw39/opt/libconfuse/share/man")
+prepend_path("PKG_CONFIG_PATH", "/home/nevikw39/opt/libconfuse/lib/pkgconfig")
+
