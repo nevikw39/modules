03f391d0c429d730d67fbcb4361a8ce69ecf1447
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-12-29 02:52:45 +0800

    SiFive Freedom RISC-V Tools

---
+++ freedom-tools.lua
@@ -0,0 +1,7 @@
+prepend_path("PATH", "/home/nevikw39/opt/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-centos6/bin")
+prepend_path("CPATH","/home/nevikw39/opt/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-centos6/include")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-centos6/lib")
+prepend_path("LD_LIBRARY_PATH","/home/nevikw39/opt/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-centos6/lib64")
+prepend_path("PKG_CONFIG_PATH","/home/nevikw39/opt/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-centos6/lib/pkgconfig")
+prepend_path("MANPATH", "/home/nevikw39/opt/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-centos6/share/man")
+
